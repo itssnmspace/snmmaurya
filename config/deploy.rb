7fe2c37b2cf29beafa7964be4dbaf010fc9fb726
Snm Maurya <snmmaurya@gmail.com>
2016-08-10 12:14:11 +0530

    initialize

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+# config valid only for current version of Capistrano
+lock '3.6.0'
+
+set :application, 'snmmaurya'
+set :repo_url, 'git@github.com:snmspace/snmmaurya.git' # change this to your git server
+
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+set :use_sudo, false
+set :bundle_binstubs, nil
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+after 'deploy:publishing', 'deploy:restart'
+
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:stop'
+    invoke 'unicorn:reload'
+  end
+
+  task :stop do
+    invoke 'unicorn:stop'
+  end
+end
