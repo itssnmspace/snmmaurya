903fedc57b4a2db957a20f881e7302e8bfab3cfb
Snm Maurya <snmmaurya@gmail.com>
2016-08-22 20:12:39 +0530

    config

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,7 @@ module Snmmaurya
     # -- all .rb files in that directory are automatically loaded.
 
     # Enable the asset pipeline
-    config.assets.enabled = true
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
