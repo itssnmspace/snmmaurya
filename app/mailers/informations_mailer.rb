e7eff0419b85c4829ac8b61c8a3730669e79a84f
Snm Maurya <snmmaurya@gmail.com>
2016-08-24 14:43:29 +0530

    mailer on problem creation

--- app/mailers/informations_mailer.rb
+++ app/mailers/informations_mailer.rb
@@ -4,4 +4,9 @@ class InformationsMailer < ApplicationMailer
     @options = options
     mail(to: @options[:to], from: @options[:from], subject: @options[:subject], content_type: "text/html")
   end
+
+  def problem options={}
+    @options = options
+    mail(to: Settings.administrator_emails.informations, from: @options[:user].email, subject: @options[:subject], content_type: "text/html")
+  end
 end
