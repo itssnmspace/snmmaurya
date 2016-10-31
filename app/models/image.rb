7eced942e3d17c1ed41a6767bc482766f76b4e8f
Snm Maurya <snmmaurya@gmail.com>
2016-10-31 19:29:33 +0530

    original uploaded image

--- app/models/image.rb
+++ app/models/image.rb
@@ -4,11 +4,11 @@ class Image < ApplicationRecord
   has_attached_file :data,
     :storage => :cloudinary,
     :cloudinary_credentials => Rails.root.join("config/cloudinary.yml"),
-    styles: { medium: "300x300>", thumb: "100x100>", original: ["100%"] },
+    styles: { medium: "300x300>", thumb: "100x100>", original: ["100%"], profile: "470x250>" },
     #Attention please specify path and url
     :path => 'uploads/image_assets/:id/:filename.:extension',
     :url => '/:class/:id/:filename.:extension'
 
 
   validates_attachment_content_type :data, content_type: /\Aimage\/.*\z/
-end
+endprofile: "470"
