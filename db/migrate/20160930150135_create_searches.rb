d88fe53a4ad5aa5e848faacf25e19f1e7ea8f441
Snm Maurya <snmmaurya@gmail.com>
2017-04-22 20:58:12 +0530

    migration fix

--- db/migrate/20160930150135_create_searches.rb
+++
@@ -1,9 +0,0 @@
-class CreateSearches < ActiveRecord::Migration[5.0]
-  def change
-    create_table :searches do |t|
-      t.string :title
-      t.text :description
-      t.timestamps
-    end
-  end
-end
